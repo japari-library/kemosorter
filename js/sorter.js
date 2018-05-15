8bf135c4b5887b30f44970f1181ea1f6c5642373
Siddiq <devsfiq@gmail.com>
2018-05-16 02:07:20 +0800

    Update sorter.js

--- js/sorter.js
+++ js/sorter.js
@@ -135,7 +135,7 @@ function start(characters = null) {
     /** Convert boolean array form to string form. */
 
     characterDataToSort = characterData.filter(char => {
-        return char.categories.some(category => optTaken.includes(category));
+        return char.categories.some(category => optTaken.includes(category._id));
     });
 
     if (characterDataToSort.length < 2) {
